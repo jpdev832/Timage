7db5df2bd41a0ea2db6e433cbbdbdf59b2c7d9df
jpdev832 <jpdev832@gmail.com>
2013-06-09 17:42:12 -0400

    Adding ImageWriter logic and generic comment class
    comment class is used by gson to create json string

---
+++ android/Timage/src/com/staticvillage/timage/utils/CommentData.java
@@ -0,0 +1,38 @@
+package com.staticvillage.timage.utils;
+
+public class CommentData {
+	private String appId;
+	private int appVersion;
+	private float[] orientation;
+	
+	public CommentData(){
+		orientation = new float[]{ 0, 0, 0 };
+	}
+	
+	public String getAppId() {
+		return appId;
+	}
+	
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	
+	public int getAppVersion() {
+		return appVersion;
+	}
+	
+	public void setAppVersion(int appVersion) {
+		this.appVersion = appVersion;
+	}
+	
+	public float[] getOrientation() {
+		return orientation;
+	}
+	
+	public void setOrientation(float[] orientation) {
+		if(orientation.length == 3)
+			this.orientation = orientation;
+	}
+	
+	
+}
--- android/Timage/src/com/staticvillage/timage/utils/ImageWriter.java
+++ android/Timage/src/com/staticvillage/timage/utils/ImageWriter.java
@@ -1,7 +1,94 @@
 package com.staticvillage.timage.utils;
 
+import java.io.IOException;
+import java.util.Date;
+
+import com.google.gson.Gson;
+
 import android.media.ExifInterface;
 
+/**
+ * Exif data writer for Timage compatible images
+ * 
+ * @author Joel
+ */
 public class ImageWriter {
-	private ExifInterface mExif;
+	public static final String COMMENT_TAG = "UserComment";
+	
+	private float[] orientation;
+	private float[] location;
+	private long timestamp;
+	
+	public ImageWriter(){
+		orientation = new float[]{ 0, 0, 0 };
+		location = new float[]{ 0, 0 };
+		timestamp = (new Date()).getTime();
+	}
+	
+	/**
+	 * Set gyroscope orientation data for image
+	 * 
+	 * @param orientation
+	 * @throws Exception
+	 */
+	public void setOrientation(float[] orientation) throws Exception{
+		if(orientation.length != 3)
+			throw new Exception("TODO define exception");
+		
+		this.orientation = orientation;
+	}
+	
+	/**
+	 * Set GPS location for image
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 */
+	public void setLocation(float latitude, float longitude){
+		location[0] = latitude;
+		location[1] = longitude;
+	}
+	
+	/**
+	 * Set timestamp for image
+	 * 
+	 * @param timestamp Time/Date image was captured
+	 */
+	public void setTimestamp(long timestamp){
+		this.timestamp = timestamp;
+	}
+	
+	/**
+	 * Write attributes to image
+	 * 
+	 * @param imageFile
+	 * @throws IOException
+	 */
+	public void write(String imageFile) throws IOException{
+		ExifInterface exif = new ExifInterface(imageFile);
+		exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, String.valueOf(location[0]));
+		exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, String.valueOf(location[1]));
+		exif.setAttribute(ExifInterface.TAG_DATETIME, String.valueOf(timestamp));
+		exif.setAttribute(COMMENT_TAG, orientationToJson("", 0, orientation));
+		exif.saveAttributes();
+	}
+	
+	/**
+	 * Generate json data for Timage user comment data
+	 * 
+	 * @param appId	application UID
+	 * @param version application version
+	 * @param orientation image orientation
+	 * @return Timage comment data
+	 */
+	public static String orientationToJson(String appId, int version, float[] orientation){
+		//TODO - define definite json structure
+		CommentData comment = new CommentData();
+		comment.setAppId(appId);
+		comment.setAppVersion(version);
+		comment.setOrientation(orientation);
+		
+		Gson gson = new Gson();
+		return gson.toJson(comment);
+	}
 }
